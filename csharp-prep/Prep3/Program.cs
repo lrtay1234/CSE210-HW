7120e59bddeabe45e988cbdb2bc0d7cf6f4ddabb
lrtay123 <lrtay123@yahoo.com>
2024-09-19 14:49:07 -0800

    Guessing game complete

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,27 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        int guessing = 1;
+        int number = 0;
+
+        Console.Write("What is the  magic number? ");
+        string magic = Console.ReadLine();
+        number = int.Parse(magic);
+
+        while (guessing != number)
+        {  
+
+            Console.Write("What is your guess? ");
+            string guess = Console.ReadLine();
+            guessing = int.Parse(guess);
+
+
+            if (guessing < number)
+                Console.WriteLine("Higher ");
+            else if (guessing > number)
+                Console.WriteLine("Lower");
+        
+        }
+        Console.WriteLine("You got it!! ");
     }
 }
