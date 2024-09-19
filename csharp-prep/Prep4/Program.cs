454bb11f89d74f05d73f40aa7222b5d207ad2634
lrtay123 <lrtay123@yahoo.com>
2024-09-19 15:02:26 -0800

    lists project

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,9 +1,48 @@
 using System;
+using System.Collections.Generic;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        List<int> numbers = new List<int>();
+
+        int Number = -1;
+        while (Number != 0)
+        {
+            Console.Write("Enter a number (0 to quit): ");
+            
+            string userResponse = Console.ReadLine();
+            Number = int.Parse(userResponse);
+            if (Number != 0)
+            {
+                numbers.Add(Number);
+            }
+        }
+        int sum = 0;
+        //Takes each number in the list and adds them up
+        foreach (int number in numbers)
+        {
+            sum += number;
+        }
+
+        Console.WriteLine($"The sum is: {sum}");
+
+        // here takes the total sum of the list, and then takes the amount of variables in the list and divides to get the average
+        float average = ((float)sum) / numbers.Count;
+        Console.WriteLine($"The average is: {average}");
+
+
+        int max = numbers[0];
+
+        foreach (int number in numbers)
+        {
+            if (number > max)
+            {
+                max = number;
+            }
+        }
+
+        Console.WriteLine($"The max is: {max}");
     }
 }
