88978bed60b10af376c22af0235f9fda4ef7d33f
lrtay123 <lrtay123@yahoo.com>
2024-09-23 08:39:41 -0800

    made changes

---
+++ prepare/Learning02/Job.cs
@@ -0,0 +1,10 @@
+using System;
+public class Job;
+{
+    public string _jobTitle;
+    public string _company;
+    public string _stateStart;
+    public string _jobFinish;
+
+
+}
--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -1,9 +1,21 @@
 using System;
-
-class Program
+public class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        Job job1 = new Job();
+        job1._jobTitle = "Engineer";
+        job1._company = "Microsoft";
+        Console.WriteLine("Job 1 Company: " + job1._company);
+
+        Job job2 = new Job();
+        job2._jobTitle = "Engineer";
+        job2._company = "Microsoft";
+        Console.WriteLine("Job 2");
+
+
+
+
+        
     }
 }
